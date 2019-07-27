139b224543ba45c4551e6eba4c31623af279afbd
Emil Bengtsson <emil@emil0r.com>
2019-07-27 13:42:58 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject ez_plugin "0.1.0-SNAPSHOT"
+
+  :description "Library for creating a plugin system."
+
+  :url "https://github.com/emil0r/ez-plugin/"
+
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+
+  :repl-options {:init-ns ez-plugin.core})
---
+++ test/ez_plugin/core_test.clj
@@ -0,0 +1,3 @@
+(ns ez-plugin.core-test
+  (:require [clojure.test :refer :all]
+            [ez-plugin.core :refer :all]))
