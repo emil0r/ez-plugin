eaba576c97dc7e11ceacc4867d6160d434af19b3
Emil Bengtsson <emil@emil0r.com>
2019-07-27 14:45:49 +0200

    test added

--- test/ez_plugin/core_test.clj
+++ test/ez_plugin/core_test.clj
@@ -1,3 +1,8 @@
 (ns ez-plugin.core-test
   (:require [clojure.test :refer :all]
             [ez-plugin.core :refer :all]))
+
+(deftest a-test
+  (testing "Load plugin.edn"
+    (is (= [#:ez-plugin-test1.core{:hook :loaded} #:ez-plugin-test2.core{:hook :loaded}]
+           (load-plugins "plugin.edn" {})))))
