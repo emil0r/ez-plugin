e1645cb4d5fcc4c0afbfdaf847432c9d15a63d8d
Emil Bengtsson <emil@emil0r.com>
2019-07-27 14:40:23 +0200

    changed to dash in project names

--- ez_plugin_test1/project.clj
+++ ez_plugin_test1/project.clj
@@ -1,4 +1,4 @@
-(defproject ez_plugin_test1 "0.1.0-SNAPSHOT"
+(defproject ez-plugin-test1 "0.1.0-SNAPSHOT"
 
   :dependencies [[org.clojure/clojure "1.10.0"]]
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ez_plugin "0.1.0-SNAPSHOT"
+(defproject ez-plugin "0.1.0-SNAPSHOT"
 
   :description "Library for creating a plugin system."
 
@@ -8,5 +8,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
   :dependencies [[org.clojure/clojure "1.10.0"]]
+
+  :profiles {:dev {:dependencies [[ez-plugin-test1 "0.1.0-SNAPSHOT"]
+                                  [ez-plugin-test2 "0.1.0-SNAPSHOT"]]}}
 
   :repl-options {:init-ns ez-plugin.core})
